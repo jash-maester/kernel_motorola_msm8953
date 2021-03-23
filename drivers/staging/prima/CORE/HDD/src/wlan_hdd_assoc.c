ead087daaeb3971386d9965eb9d04db728d7856a
marcost2 <marcostolcachir@gmail.com>
2021-03-23 10:43:14 -0300

    drivers:staging:prima: Downgrade to LA.UM.8.6.r1-02600-89xx.0

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_assoc.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_assoc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -801,13 +801,8 @@ static void hdd_copy_vht_operation(hdd_station_ctx_t *hdd_sta_ctx,
     vos_mem_zero(hdd_vht_ops, sizeof(struct ieee80211_vht_operation));
 
     hdd_vht_ops->chan_width = roam_vht_ops->chanWidth;
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0))
-    hdd_vht_ops->center_freq_seg0_idx = roam_vht_ops->chanCenterFreqSeg1;
-    hdd_vht_ops->center_freq_seg1_idx = roam_vht_ops->chanCenterFreqSeg2;
-#else
     hdd_vht_ops->center_freq_seg1_idx = roam_vht_ops->chanCenterFreqSeg1;
     hdd_vht_ops->center_freq_seg2_idx = roam_vht_ops->chanCenterFreqSeg2;
-#endif
     hdd_vht_ops->basic_mcs_set = roam_vht_ops->basicMCSSet;
 }
 
@@ -957,10 +952,10 @@ static void hdd_SendFTAssocResponse(struct net_device *dev, hdd_adapter_t *pAdap
     unsigned int len = 0;
     u8 *pFTAssocRsp = NULL;
 
-    if (pCsrRoamInfo->nAssocRspLength < FT_ASSOC_RSP_IES_OFFSET)
+    if (pCsrRoamInfo->nAssocRspLength == 0)
     {
         hddLog(LOGE,
-            "%s: Invalid assoc rsp length %d",
+            "%s: pCsrRoamInfo->nAssocRspLength=%d",
             __func__, (int)pCsrRoamInfo->nAssocRspLength);
         return;
     }
@@ -979,16 +974,6 @@ static void hdd_SendFTAssocResponse(struct net_device *dev, hdd_adapter_t *pAdap
         (unsigned int)pFTAssocRsp[0],
         (unsigned int)pFTAssocRsp[1]);
 
-    /* Send the Assoc Resp, the supplicant needs this for initial Auth. */
-    len = pCsrRoamInfo->nAssocRspLength - FT_ASSOC_RSP_IES_OFFSET;
-    if (len > IW_GENERIC_IE_MAX) {
-        hddLog(LOGE,
-             "%s: Invalid assoc rsp length %d",
-             __func__, (int)pCsrRoamInfo->nAssocRspLength);
-        return;
-    }
-    wrqu.data.length = len;
-
     // We need to send the IEs to the supplicant.
     buff = kmalloc(IW_GENERIC_IE_MAX, GFP_ATOMIC);
     if (buff == NULL)
@@ -997,6 +982,9 @@ static void hdd_SendFTAssocResponse(struct net_device *dev, hdd_adapter_t *pAdap
         return;
     }
 
+    // Send the Assoc Resp, the supplicant needs this for initial Auth.
+    len = pCsrRoamInfo->nAssocRspLength - FT_ASSOC_RSP_IES_OFFSET;
+    wrqu.data.length = len;
     memset(buff, 0, IW_GENERIC_IE_MAX);
     memcpy(buff, pFTAssocRsp, len);
     wireless_send_event(dev, IWEVASSOCRESPIE, &wrqu, buff);
@@ -2243,10 +2231,8 @@ static void hdd_SendReAssocEvent(struct net_device *dev, hdd_adapter_t *pAdapter
         goto done;
     }
 
-    if (pCsrRoamInfo->nAssocRspLength < FT_ASSOC_RSP_IES_OFFSET) {
-
-        hddLog(LOGE, "%s: Invalid assoc response length %d",
-               __func__, pCsrRoamInfo->nAssocRspLength);
+    if (pCsrRoamInfo->nAssocRspLength == 0) {
+        hddLog(LOGE, "%s: Invalid assoc response length", __func__);
         goto done;
     }
 
@@ -2263,11 +2249,6 @@ static void hdd_SendReAssocEvent(struct net_device *dev, hdd_adapter_t *pAdapter
 
     // Send the Assoc Resp, the supplicant needs this for initial Auth.
     len = pCsrRoamInfo->nAssocRspLength - FT_ASSOC_RSP_IES_OFFSET;
-    if (len > IW_GENERIC_IE_MAX) {
-        hddLog(LOGE, "%s: Invalid assoc response length %d",
-                __func__, pCsrRoamInfo->nAssocRspLength);
-         goto done;
-    }
     rspRsnLength = len;
     memcpy(rspRsnIe, pFTAssocRsp, len);
     memset(rspRsnIe + len, 0, IW_GENERIC_IE_MAX - len);
@@ -4669,7 +4650,6 @@ static tANI_S32 hdd_ProcessGENIE(hdd_adapter_t *pAdapter,
        flag to 0 */
     memset( &dot11WPAIE, 0 , sizeof(tDot11fIEWPA) );
     memset( &dot11RSNIE, 0 , sizeof(tDot11fIERSN) );
-    memset( PMKIDCache, 0 , sizeof(tPmkidCacheInfo) * 4);
 
     // Type check
     if ( gen_ie[0] ==  DOT11F_EID_RSN)
--- drivers/staging/prima/CORE/SME/src/csr/csrUtil.c
+++ drivers/staging/prima/CORE/SME/src/csr/csrUtil.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017, 2019-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2017, 2019 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -3809,33 +3809,6 @@ static void csr_check_sae_auth(tpAniSirGlobal mac_ctx,
 }
 #endif
 
-bool csr_is_pmkid_found_for_peer(tpAniSirGlobal mac,
-                                tCsrRoamSession *session,
-                                tSirMacAddr peer_mac_addr,
-                                uint8_t *pmkid, uint16_t pmkid_count)
-{
-    uint32_t i, index;
-    uint8_t *session_pmkid;
-    tPmkidCacheInfo pmkid_cache;
-
-    vos_mem_zero(&pmkid_cache, sizeof(pmkid_cache));
-    vos_mem_copy(pmkid_cache.BSSID, peer_mac_addr, VOS_MAC_ADDR_SIZE);
-
-    if (!csr_lookup_pmkid_using_bssid(mac, session, &pmkid_cache, &index))
-        return false;
-    session_pmkid = &session->PmkidCacheInfo[index].PMKID[0];
-        for (i = 0; i < pmkid_count; i++) {
-            if (vos_mem_compare(pmkid + (i * CSR_RSN_PMKID_SIZE),
-                session_pmkid, CSR_RSN_PMKID_SIZE))
-                return true;
-            }
-
-    VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_DEBUG,
-            "PMKID in PmkidCacheInfo doesn't match with PMKIDs of peer");
-
-    return false;
-}
-
 tANI_BOOLEAN csrGetRSNInformation( tHalHandle hHal, tCsrAuthList *pAuthType, eCsrEncryptionType enType, tCsrEncryptionList *pMCEncryption,
                                    tDot11fIERSN *pRSNIe,
                            tANI_U8 *UnicastCypher,
@@ -4122,6 +4095,39 @@ tANI_BOOLEAN csrIsRSNMatch( tHalHandle hHal, tCsrAuthList *pAuthType,
     return( fRSNMatch );
 }
 
+/**
+ * csr_lookup_pmkid_using_ssid() - lookup pmkid using ssid and cache_id
+ * @mac: pointer to mac
+ * @session: sme session pointer
+ * @pmk_cache: pointer to pmk cache
+ * @index: index value needs to be seached
+ *
+ * Return: true if pmkid is found else false
+ */
+static bool csr_lookup_pmkid_using_ssid(tpAniSirGlobal mac,
+                    tCsrRoamSession *session,
+                    tPmkidCacheInfo *pmk_cache,
+                    uint32_t *index)
+{
+    uint32_t i;
+    tPmkidCacheInfo *session_pmk;
+
+    for (i = 0; i < session->NumPmkidCache; i++) {
+        session_pmk = &session->PmkidCacheInfo[i];
+
+        if ((!vos_mem_compare(pmk_cache->ssid, session_pmk->ssid,
+                  pmk_cache->ssid_len)) &&
+            (!vos_mem_compare(session_pmk->cache_id,
+                  pmk_cache->cache_id, CACHE_ID_LEN))) {
+            /* match found */
+            *index = i;
+            return true;
+        }
+    }
+
+    return false;
+}
+
 bool csr_lookup_pmkid_using_bssid(tpAniSirGlobal mac,
                     tCsrRoamSession *session,
                     tPmkidCacheInfo *pmk_cache,
@@ -4144,7 +4150,7 @@ bool csr_lookup_pmkid_using_bssid(tpAniSirGlobal mac,
 }
 
 tANI_BOOLEAN csrLookupPMKID(tpAniSirGlobal pMac, tANI_U32 sessionId,
-                            tPmkidCacheInfo *pmk_cache)
+                                tPmkidCacheInfo *pmk_cache)
 
 {
     tANI_BOOLEAN fRC = FALSE, fMatchFound = FALSE;
@@ -4165,20 +4171,22 @@ tANI_BOOLEAN csrLookupPMKID(tpAniSirGlobal pMac, tANI_U32 sessionId,
         pSession->fIgnorePMKIDCache = FALSE;
         return fRC;
     }
+    
+   if (pmk_cache->ssid_len) {
+       /* Try to find based on cache_id and ssid first */
+       fMatchFound = csr_lookup_pmkid_using_ssid(pMac, pSession, pmk_cache,
+                                                 &Index);
+   }
 
     /* If not able to find using cache id or ssid_len is not present */
-    fMatchFound = csr_lookup_pmkid_using_bssid(pMac, pSession, pmk_cache,
-                                               &Index);
+    if (!fMatchFound)
+        fMatchFound = csr_lookup_pmkid_using_bssid(pMac, pSession, pmk_cache,
+                                                   &Index);
 
    if (!fMatchFound) {
        smsLog(pMac, LOG2, "No PMKID Match Found");
        return false;
-   }
 
-   if (pSession->PmkidCacheInfo[Index].pmk_len > CSR_RSN_MAX_PMK_LEN) {
-        smsLog(pMac, LOG2, "PMK length %d is invalid",
-               pSession->PmkidCacheInfo[Index].pmk_len);
-        return false;
    }
 
    vos_mem_copy(pmk_cache->PMKID, pSession->PmkidCacheInfo[Index].PMKID, CSR_RSN_PMKID_SIZE);
@@ -4217,8 +4225,6 @@ tANI_U8 csrConstructRSNIe( tHalHandle hHal, tANI_U32 sessionId, tCsrRoamProfile 
     tDot11fIERSN rsn_ie = {0};
 
     smsLog(pMac, LOGW, "%s called...", __func__);
-
-    vos_mem_zero(&pmkid_cache, sizeof(pmkid_cache));
 
     do
     {
